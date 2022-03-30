863d3ddeff00f92a30b644ed8b8bba058179aa85
IgorBarberino <48576880+IgorBarberino@users.noreply.github.com>
2022-03-30 20:54:59 -0300

    Update defender.c

--- src/defender.c
+++ src/defender.c
@@ -8,8 +8,9 @@
 
 // Main header
 #include "defender.h"
-direction_t last_direction = DIR_RIGHT;
-direction_t NEW_direction = DIR_UP_RIGHT;
+direction_t last_direction = DIR_UP;
+direction_t second_last_direction = DIR_DOWN;
+direction_t new_direction = DIR_UP_DOWN;
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
@@ -21,11 +22,14 @@ direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   UNUSED(defender_position);
   UNUSED(attacker_spy);
+  second_last_direction = last_direction;
   last_direction = new_direction;
-  if (last_direction == DIR_RIGHT)
-      new_direction = DIR_UP_RIGHT;
-  else
-      new_direction = DIR_RIGHT;
+  if (last_direction == DIR_UP && second_last_direction == DIR_UP)
+      new_direction = DIR_DOWN;
+  else if (last_direction == DIR_DOWN && second_last_direction == DIR_DOWN)
+      new_direction = DIR_UP;
+  else 
+      new_direction = last_direction;
   return (new_direction);
 }
 
